f9c90e3681f2c656646df5f277e6094a04daba52
sarktus <tstusharsarkar18@gmail.com>
2020-05-10 19:18:07 +0530

    Azure function Added

---
+++ BusinessStandard.Api/Controllers/AzureFunctionInvockController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using BusinessStandard.Api.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessStandard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AzureFunctionInvockController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AzureFunctionInvockController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get(string message)
+        {
+            AzureFunctionInvokeHelper helper = new AzureFunctionInvokeHelper(_configuration);
+            var response = await helper.InvokeFunction(message);
+            return Ok(response);
+        }
+
+    }
+}
---
+++ BusinessStandard.Api/Resources/AzureFunctionInvokeHelper.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusinessStandard.Api.Resources
+{
+    public class AzureFunctionInvokeHelper
+    {
+        private string AzureFuntionUrl;
+        public AzureFunctionInvokeHelper(IConfiguration configuration)
+        {
+            AzureFuntionUrl = configuration.GetValue<string>("AppSettings:AzureFunctionURL");
+        }
+
+        /// <summary>
+        /// Invoke function
+        /// </summary>
+        /// <param name="message">string message</param>
+        /// <returns>string message</returns>
+        public async Task<string> InvokeFunction(string message)
+        {
+            string functionResponse = string.Empty;
+            dynamic content = new { name = message };
+
+            CancellationToken cancellationToken;
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage(HttpMethod.Post, AzureFuntionUrl))
+            using (var httpContent = CreateHttpContent(content))
+            {
+                request.Content = httpContent;
+                using (var response = await client
+                    .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+                    .ConfigureAwait(false))
+                {
+                    functionResponse = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            return functionResponse;
+        }
+
+        /// <summary>
+        /// Serialize Json Into Stream
+        /// </summary>
+        /// <param name="value">Json Data</param>
+        /// <param name="stream">stream</param>
+        private void SerializeJsonIntoStream(object value, Stream stream)
+        {
+            using (var sw = new StreamWriter(stream, new UTF8Encoding(false), 1024, true))
+            using (var jtw = new JsonTextWriter(sw) { Formatting = Formatting.None })
+            {
+                var js = new JsonSerializer();
+                js.Serialize(jtw, value);
+                jtw.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Creating httpcontent
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        private HttpContent CreateHttpContent(object content)
+        {
+            HttpContent httpContent = null;
+            if (content != null)
+            {
+                var ms = new MemoryStream();
+                SerializeJsonIntoStream(content, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                httpContent = new StreamContent(ms);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+            return httpContent;
+        }
+    }
+}
